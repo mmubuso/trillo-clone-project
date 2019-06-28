3c9fd41ead25c11a34efeb647c0fc85a87cebd71
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-28 15:16:00 -0400

    Setup MongoDb, add schema for account data

---
+++ models/account.js
@@ -0,0 +1,59 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const AccountSchema = new mongoose.Schema({
+ name: {
+   type: String,
+   required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  boardObjectId: ObjectId,
+  numberOfBoards: Number
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const AccountCollection = mongoose.model('Account', AccountSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+
+function getAllAcounts() {
+  return AccountCollection.find()
+}
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  getAllAcounts
+}
