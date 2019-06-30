ff5c9a65a066e2761174a57873c3201e1fbbbb3f
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-30 17:03:46 -0400

    add function and route to retrive all lists

---
+++ models/list.js
@@ -0,0 +1,60 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const ListSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  numberOfCards: {
+    type: Number,
+    default: 0
+  },
+  boardObjectId: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  }
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const ListCollection = mongoose.model('List', ListSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+function getAllListsByBoardId(boardId) {
+  return ListCollection.find({boardObjectId: boardId})
+}
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  getAllListsByBoardId
+}
