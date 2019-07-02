10a0db2ddd4c728fd72b4146db191f771e11455f
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-02 11:41:09 -0400

    create function to format cards data into useable format for handlebars

---
+++ models/formatCardsArray.js
@@ -0,0 +1,22 @@
+/* Created this script to handle formatting the nested array we recieve from the cards function in board controller */
+
+//Thif function iterates through a 2 dimensional array and pushes the parameter into an empty array
+//Params 
+//arr - a  2 dimensional array from the board controller
+function createNewObj(arr) {
+    let cardsArray = []
+    if (cardsArray.length === 0) {
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+          cardsArray.push(arr[i][j])
+        }
+      }
+    }
+    return cardsArray
+  }
+  
+  
+  module.exports = {
+    createNewObj
+  }
+  
